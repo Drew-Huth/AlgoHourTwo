609849b128f0b2c10c57daefaef004d6488b2c02
Huth <drew.huth@fiserv.com>
2023-10-10 15:47:49 -0400

    attempt 2

--- Solution.java
+++ Solution.java
@@ -22,7 +22,7 @@ public class Solution {
             if (num<=1){
                 return false;
             }
-            for (int i = 2; i<= Math.sqrt(num)/2;i++){
+            for (int i = 2; i<= Math.sqrt(num);i++){
                 if (num%i == 0) return false;
             }
             return true;
